a5defcb85953a00f1dd47260270577a237d38bb2
kimminseok <mikim@student.42.us.org>
2019-10-10 14:09:53 -0700

    Leetcode: Daily

---
+++ easy/1071.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   1071.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mikim <mikim@student.42.us.org>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/10 14:09:11 by mikim             #+#    #+#             */
+/*   Updated: 2019/10/10 14:09:42 by mikim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                              Mingyun Kim   */
+/*                                           https://www.github.com/mikim42   */
+/* ************************************************************************** */
+
+/*
+**	LeetCode:  1071. Greatest Common Divisor of Strings [easy]
+*/
+
+class Solution {
+	public:
+		string gcdOfStrings(string str1, string str2) {
+			while (!str2.empty()) {
+				if (str1.size() < str2.size())
+					swap(str1, str2);
+				if (str1.substr(0, str2.size()) != str2)
+					return "";
+				str1 = str1.substr(str2.size());
+			}
+			return str1;
+		}
+};
