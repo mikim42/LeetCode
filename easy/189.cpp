2a681bb5eab275b0676206e54c37fbec16a24506
kimminseok <mikim@student.42.us.org>
2019-10-22 11:23:11 -0700

    Leetcode: Daily, Header fix

--- easy/189.cpp
+++ easy/189.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   2.cpp                                              :+:      :+:    :+:   */
+/*   189.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mikim <mikim@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/22 11:22:50 by mikim             #+#    #+#             */
-/*   Updated: 2019/10/22 11:22:51 by mikim            ###   ########.fr       */
+/*   Created: 2019/10/22 11:21:03 by mikim             #+#    #+#             */
+/*   Updated: 2019/10/22 11:21:05 by mikim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,39 +16,23 @@
 /* ************************************************************************** */
 
 /*
-**	LeetCode: 2. Add Two Numbers [medium]
+**	LeetCode: 189. Rotate Array [easy]
 */
 
 class Solution {
 	public:
-		ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-			ListNode *res = NULL;
-			ListNode *it = NULL;
-			ListNode *tmp = NULL;
-			int carry = 0;
+		void rotate(vector<int>& nums, int k) {
+			queue<int> q;
+			int n = nums.size();
+			k %= n;
 
-			while (l1 || l2 || carry) {
-				tmp = new ListNode(carry);
-				carry = 0;
-				if (l1) {
-					tmp->val += l1->val;
-					l1 = l1->next;
-				}
-				if (l2) {
-					tmp->val += l2->val;
-					l2 = l2->next;
-				}
-				carry = tmp->val >= 10 ? 1 : 0;
-				tmp->val %= 10;
-				if (!res) {
-					res = tmp;
-					it = res;
-				}
-				else {
-					it->next = tmp;
-					it = it->next;
-				}
+			for (int i = 0; i < n; i++) {
+				if (i < n - k)
+					q.push(nums[i]);
+				if (i < k)
+					nums[i] = nums[n - k + i];
+				else
+					nums[i] = q.front(), q.pop();
 			}
-			return res;
 		}
 };
