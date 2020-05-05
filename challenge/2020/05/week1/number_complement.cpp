1ab2ff89aa995381bca3fe253bda4d848acde108
Mingyun (KJ) Kim <mingkim0210@gmail.com>
2020-05-05 10:58:43 -0700

    LeetCode: Daily

---
+++ challenge/2020/05/week1/number_complement.cpp
@@ -0,0 +1,9 @@
+class Solution {
+	public:
+		int findComplement(int num) {
+			int res = 0, exp = 0;
+			if (num == 0) return 1;
+			for (int n = num; n > 0; n /= 2) res += (1 ^ n % 2) * pow(2, exp++);
+			return res;
+		}
+};
