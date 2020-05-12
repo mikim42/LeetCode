106d6dd1826f35fd9c018e86ceec613685929875
Mingyun (KJ) Kim <mingkim0210@gmail.com>
2020-05-12 12:41:44 -0700

    LeetCode: Daily

---
+++ medium/540.cpp
@@ -0,0 +1,19 @@
+/*
+**	 540. Single Element in a Sorted Array [medium]
+*/
+
+class Solution {
+	public:
+		int singleNonDuplicate(vector<int>& nums) {
+			int l = 0, r = nums.size() - 1;
+
+			while (l < r) {
+				int m = l + (r - l) / 2;
+				if (nums[m] == nums[m ^ 1])
+					l = m + 1;
+				else
+					r = m;
+			}
+			return nums[l];
+		}
+};
