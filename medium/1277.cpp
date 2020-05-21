deb7101b7f41b3b31cd54cb7587fabcbf38eb43f
Mingyun (KJ) Kim <mingkim0210@gmail.com>
2020-05-21 15:04:51 -0700

    LeetCode: Daily

---
+++ medium/1277.cpp
@@ -0,0 +1,21 @@
+/*
+**	LeetCode: 1277. Count Square Submatrices with All Ones [medium]
+*/
+
+class Solution {
+	public:
+		int countSquares(vector<vector<int>>& mat) {
+			int n = mat.size();
+			int m = mat[0].size();
+			int res = 0;
+
+			for (int i = 0; i < n; ++i) {
+				for (int j = 0; j < m; ++j) {
+					if (mat[i][j] != 0 && i > 0 && j > 0)
+						mat[i][j] += min({mat[i][j - 1], mat[i - 1][j], mat[i -1][j -1]});
+					res += mat[i][j];
+				}
+			}
+			return res;
+		}
+};;
