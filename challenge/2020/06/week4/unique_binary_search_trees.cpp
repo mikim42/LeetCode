e3c779da12862c9fe773169edf8262126f7b7868
Mingyun (KJ) Kim <mingkim0210@gmail.com>
2020-06-24 12:38:06 -0700

    LeetCode: Daily

---
+++ challenge/2020/06/week4/unique_binary_search_trees.cpp
@@ -0,0 +1,16 @@
+class Solution {
+	private:
+		double fact(int n) {
+			double res = 1;
+			while (n > 1)
+				res *= n--;
+			return res;
+		}
+
+	public:
+		int numTrees(int n) {
+			return round(fact(2 * n) / (fact(n) * fact(n) * (n + 1)));
+		}
+};
+
+https://leetcode.com/problems/unique-binary-search-trees/discuss/703049/Python-Math-oneliner-O(n)-using-Catalan-number-explained
